e2ebff573bbc0e870470abefe3a957937f7d0fc5
shrekle <nanonline760@gmail.com>
2022-08-17 19:05:14 -0800

    Initial Commit

---
+++ H4KOR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4KOR News
+//
+//  Created by adrian garcia on 8/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4KOR News/H4KOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4KOR_NewsApp.swift
+//  H4KOR News
+//
+//  Created by adrian garcia on 8/17/22.
+//
+
+import SwiftUI
+
+@main
+struct H4KOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
